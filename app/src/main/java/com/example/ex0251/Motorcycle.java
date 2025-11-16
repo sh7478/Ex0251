e337e0a205bc7aab364f73b247286182c6b3180b
sh7478 <sh7478@bs.amalnet.k12.il>
2025-11-16 20:54:20 +0200

    Added Motorcycle class

---
+++ app/src/main/java/com/example/ex0251/Motorcycle.java
@@ -0,0 +1,18 @@
+package com.example.ex0251;
+
+public class Motorcycle extends Vehicle{
+    @Override
+    public void start() {
+        System.out.println("The motorcycle engine has started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("The rider used the brakes and put feet down");
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println("The rider accelerated with the throttle");
+    }
+}
