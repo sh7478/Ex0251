b7f0542116a1a96fac6332d8d1822e785bac3ef5
sh7478 <sh7478@bs.amalnet.k12.il>
2025-11-16 20:55:17 +0200

    Added Bicycle class

---
+++ app/src/main/java/com/example/ex0251/Bicycle.java
@@ -0,0 +1,18 @@
+package com.example.ex0251;
+
+public class Bicycle extends Vehicle{
+    @Override
+    public void start() {
+        System.out.println("The rider started pedalling");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("The rider pedalled the opposite way");
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println("The rider is pedaling faster");
+    }
+}
